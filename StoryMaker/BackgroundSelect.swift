863b84f017bc45c368461ac206164ce23c87d1f2
Marina Lee <leemarina@gmail.com>
2020-04-22 14:48:45 -0700

    update from 4/22

--- StoryMaker/BackgroundSelect.swift
+++ StoryMaker/BackgroundSelect.swift
@@ -70,26 +70,92 @@ class BackgroundSelect: UIViewController {
     var label = UILabel(frame: CGRect(x: 320, y: 290, width: 500, height: 21))
 
     var helloButton = UIButton(type: UIButton.ButtonType.custom) as UIButton
+    
+    var character =  UIImageView(frame: CGRect(x: 180, y: 450, width: 100, height: 150))
+    
+    var speechBubble = UIImageView(frame: CGRect(x: 220, y: 240, width: 500, height: 300))
+    
+    var count = 0
+    
+  //  var amuseBG = UIView(imageLiteralResourceName: "amusement2")
+        
+     
+    
+    func hideButtonQuestions() {
+        questionLabel.isHidden = true
+        Button1.isHidden = true
+        Button2.isHidden = true
+        Button3.isHidden = true
+        label.isHidden = true
+    }
+
+    func showButtonQuestions() {
+        questionLabel.isHidden = false
+        Button1.isHidden = false
+        Button2.isHidden = false
+        Button3.isHidden = false
+        label.isHidden = false
+    }
+
+    func showCharacter() {
+        character.image = UIImage(named: "dog")
+        self.view.addSubview(character)
+        UIView.animate(withDuration: 1, delay: 2, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations: {
+            self.character.transform = CGAffineTransform(translationX: 0, y: -150)
+        })
+    }
+    
+    func showSpeechBubble() {
+        
+        speechBubble.image = UIImage(named: "speechBubble")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.view.addSubview(self.speechBubble)
+            self.speechBubble.alpha = 0
+            self.speechBubble.alpha = 1
+            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 3, options: [], animations: {
+                self.speechBubble.transform = CGAffineTransform(scaleX: 2, y: 2)
+            })
+        }
+    }
+    
+    func addMessageToSpeechBubble(_ message: String) {
+        self.label.isHidden = false
+        self.label.font = UIFont(name: "Rockwell", size: 18)
+        self.label.text = message
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            self.view.addSubview(self.label)
+            self.label.animate(newText: self.label.text ?? "", characterDelay: 0.07)
+        }
+    }
+    
+    func hideIntro() {
+        view.isHidden = true
+        character.isHidden = true
+        speechBubble.isHidden = true
+        helloButton.isHidden = true
+        label.isHidden = true
+        
+    
+    }
+    
+    func hideBackground() {
+        self.view.backgroundColor = #colorLiteral(red: 0.641756475, green: 0.8999739885, blue: 0.8605425358, alpha: 1)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        
+                
         questionLabel.text = questions[row]
         
         questionLabel.adjustsFontSizeToFitWidth = true
         
-        
         Button1.setImage(UIImage(named: "amusement"), for: .normal)
-        
-        
         
         Button1.imageView?.contentMode = .scaleAspectFit
         
-        /*  Button2.setTitle(answers[row][1], for: .normal)
-         
-         Button3.setTitle(answers[row][2], for: .normal) */
+        Button2.setTitle(answer0[1] as! String, for: .normal)
+        Button3.setTitle(answer0[2] as! String, for: .normal)
         
     }
     
@@ -100,82 +166,46 @@ class BackgroundSelect: UIViewController {
         row += 1
         print(row)
         
-        if row == 1 && sender == Button1 {
-            view.addBackground(imageName: "amusement2", contentMode: .scaleAspectFill)
-            
-            questionLabel.isHidden = true
-            Button1.isHidden = true
-            Button2.isHidden = true
-            Button3.isHidden = true
-            label.isHidden = true
-            
-            let imageView = UIImageView(frame: CGRect(x: 180, y: 450, width: 100, height: 150))
-            imageView.image = UIImage(named: "dog")
-            self.view.addSubview(imageView)
-            
-            
-            UIView.animate(withDuration: 1, delay: 2, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations: {
-                
-                imageView.transform = CGAffineTransform(translationX: 0, y: -150)
-                
-            })
-            
-            let speechBubble = UIImageView(frame: CGRect(x: 220, y: 240, width: 500, height: 300))
-            speechBubble.image = UIImage(named: "speechBubble")
-            self.view.addSubview(speechBubble)
-            
-            speechBubble.alpha = 0
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                
-                speechBubble.alpha = 1
-                
+        if row == 1 {
+            hideButtonQuestions()
+            if sender == Button1 {
+                view.addBackground(imageName: "amusement2", contentMode: .scaleAspectFill)
             }
             
-            UIView.animate(withDuration: 1, delay: 3.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 3, options: [], animations: {
-                
-                speechBubble.transform = CGAffineTransform(scaleX: 2, y: 2)
-                
-                
-                
-            })
-            DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
-                self.label.isHidden = false
-                self.label.font = UIFont(name: "Rockwell", size: 18)
-                self.label.text = "Welcome to the amusement park!"
-                self.view.addSubview(self.label)
-                self.label.animate(newText: self.label.text ?? "", characterDelay: 0.07)
-            }
+        showCharacter()
             
-          
+            showSpeechBubble()
+        
             
+            addMessageToSpeechBubble("Welcome to the amusement park!")
             
+        
+            
+           
             let helloImage = UIImage(named: "hello")
             
             helloButton.frame = CGRect(x: 620, y: 330, width: 72, height: 54)
             helloButton.setImage(helloImage, for: .normal)
-            
-            
-            
-            self.view.addSubview(helloButton)
-            
-           
-            helloButton.addTarget(self, action: #selector(helloButtonClicked), for: .touchUpInside)
-
-            
-            
-            helloButton.alpha = 0
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 7) {
-                self.helloButton.alpha = 1
-            }
-            
-            UIView.animate(withDuration: 1, delay: 7.5, usingSpringWithDamping: 0.6, initialSpringVelocity: 3, options: [], animations: {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 7) {
+            self.view.addSubview(self.helloButton)
+            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 3, options: [], animations: {
                 
                 self.helloButton.transform = CGAffineTransform(scaleX: 2, y: 2)
                 
                 
             })
+        }
+            helloButton.addTarget(self, action: #selector(helloButtonClicked), for: .touchUpInside)
+            
+       
+
+            //helloButton.alpha = 0
+            
+         //   helloButton.alpha = 1
+            
+            
+            
     
 
         }
@@ -194,17 +224,39 @@ class BackgroundSelect: UIViewController {
     
     
     @IBAction func helloButtonClicked(_ sender: UIButton) {
+            count += 1
+            print(count)
+        
+        if self.count == 1 {
+            
             self.label.font = UIFont(name: "Rockwell", size: 18)
+            helloButton.isHidden = true
             self.label.text = "Let's choose a character!"
             self.view.addSubview(self.label)
             self.label.animate(newText: self.label.text ?? "", characterDelay: 0.07)
+        
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             
-            let helloImage = UIImage(named: "Next button")
-            helloButton.setImage(helloImage, for: .normal)
-
-            helloButton.removeTarget(self, action: #selector(helloButtonClicked), for: .touchUpInside)
+                self.helloButton.isHidden = false
+                let helloImage = UIImage(named: "Next button")
+                self.helloButton.setImage(helloImage, for: .normal)
+            }
+        
+        }
+            if self.count == 2 {
+                 self.helloButton.removeTarget(self, action: #selector(self.helloButtonClicked), for: .touchUpInside)
+                self.hideIntro()
+                self.hideBackground()
+                self.showButtonQuestions()
+                
+            }
             
             
+            
+       
+           
+            
+        
     }
     
     
