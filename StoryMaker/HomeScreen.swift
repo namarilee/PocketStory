f7cd4c0b64a85b67e9b18f9e6d965ec77c2cc44d
Marina Lee <leemarina@gmail.com>
2021-06-13 00:24:15 -0700

    created HomeScreen file

---
+++ StoryMaker/HomeScreen.swift
@@ -0,0 +1,16 @@
+//
+//  HomeScreen.swift
+//  StoryMaker
+//
+//  Created by Marina Lee on 6/13/21.
+//  Copyright © 2021 marinalee. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import AVFoundation
+
+class HomeScreen: UIViewController {
+    
+    @IBOutlet weak var newStoryButton: UIButton!
+}
