0fe3ff88de43e181ac3c3ed7bb43e66c7bb3a829
Marina Lee <leemarina@gmail.com>
2021-06-13 00:56:06 -0700

    worked on animations for home screen buttons

--- StoryMaker/HomeScreen.swift
+++ StoryMaker/HomeScreen.swift
@@ -13,4 +13,31 @@ import AVFoundation
 class HomeScreen: UIViewController {
     
     @IBOutlet weak var newStoryButton: UIButton!
+    
+    @IBOutlet weak var minigamesButton: UIButton!
+    
+   
+    
+    @IBAction func newStoryClicked(_ sender: Any) {
+        animateButton(button: newStoryButton)
+    }
+    
+    func animateButton(button: UIButton) {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations: {
+         //   self.button2.transform = .identity
+             button.transform =  CGAffineTransform(scaleX: 1.05, y: 1.05)
+
+            self.minigamesButton.transform = CGAffineTransform(translationX: 0, y: 500)
+            button.transform = CGAffineTransform(translationX: 0, y: 500)
+            
+        })
+      //  DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+           // self.goToNextScreen()
+       // }
+    }
+    
+    func goToNextScreen() {
+        let nextViewController = self.storyboard?.instantiateViewController(withIdentifier: "BackgroundSelect") as! BackgroundSelect
+               self.present(nextViewController, animated:false, completion:nil)
+    }
 }
