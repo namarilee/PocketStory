6ad15ce2238dc0c1bd25e408ba9cd42a600c752f
Marina Lee <leemarina@gmail.com>
2021-08-15 14:44:36 -0700

    added all songs to playlist

--- StoryMaker/PlaylistViewController.swift
+++ StoryMaker/PlaylistViewController.swift
@@ -29,6 +29,36 @@ class PlaylistViewController: UIViewController {
             ViewController.instance.playSound(soundFile: "Eb ")
             showPlayImage(image: UIImage(named: "themeSongPlaying")!)
         }
+        if playlistImageView.currentImage == UIImage(named: "parkSongPlaying") {
+                //  countClick += 1
+                  ViewController.instance.pauseSound()
+                  showPausedImage(image: UIImage(named: "parkSongPaused")!)
+        }
+        if playlistImageView.currentImage == UIImage(named: "parkSongPaused") && countClick % 2 != 0 {
+                  
+                  ViewController.instance.playSound(soundFile: "dixielandost (1)")
+                  showPlayImage(image: UIImage(named: "parkSongPlaying")!)
+        }
+        if playlistImageView.currentImage == UIImage(named: "snackSongPlaying") {
+                //  countClick += 1
+                  ViewController.instance.pauseSound()
+                  showPausedImage(image: UIImage(named: "snackSongPaused")!)
+        }
+        if playlistImageView.currentImage == UIImage(named: "snackSongPaused") && countClick % 2 != 0 {
+                  
+                  ViewController.instance.playSound(soundFile: "foodcourtost")
+                  showPlayImage(image: UIImage(named: "snackSongPlaying")!)
+        }
+        if playlistImageView.currentImage == UIImage(named: "photoSongPlaying") {
+                       //  countClick += 1
+                ViewController.instance.pauseSound()
+                showPausedImage(image: UIImage(named: "photoSongPaused")!)
+               }
+        if playlistImageView.currentImage == UIImage(named: "photoSongPaused") && countClick % 2 != 0 {
+                         
+                         ViewController.instance.playSound(soundFile: "Bb major_1")
+                         showPlayImage(image: UIImage(named: "photoSongPlaying")!)
+               }
         print(countClick)
     }
     
@@ -39,6 +69,34 @@ class PlaylistViewController: UIViewController {
         ViewController.instance.playSound(soundFile: "Eb ")
         playlistImageView.setImage(UIImage(named: "themeSongPlaying"), for: .normal)
     }
+    
+    @IBAction func parkSongClicked(_ sender: UIButton) {
+        animateButton(sender)
+        animateButton(playlistImageView)
+        ViewController.instance.pauseSound()
+        ViewController.instance.playSound(soundFile: "dixielandost (1)")
+        playlistImageView.setImage(UIImage(named: "parkSongPlaying"), for: .normal)
+
+    }
+    
+    
+    @IBAction func snackSongClicked(_ sender: UIButton) {
+        animateButton(sender)
+        animateButton(playlistImageView)
+        ViewController.instance.pauseSound()
+        ViewController.instance.playSound(soundFile: "foodcourtost")
+        playlistImageView.setImage(UIImage(named: "snackSongPlaying"), for: .normal)
+    }
+    
+    @IBAction func photoSongClicked(_ sender: UIButton) {
+        animateButton(sender)
+        animateButton(playlistImageView)
+        ViewController.instance.pauseSound()
+        ViewController.instance.playSound(soundFile: "Bb major_1")
+        playlistImageView.setImage(UIImage(named: "photoSongPlaying"), for: .normal)
+    }
+    
+    
     func animateButton(_ buttonToAnimate: UIView) {
               UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
                        buttonToAnimate.transform = CGAffineTransform(scaleX: 0.92, y: 0.92)
@@ -61,4 +119,10 @@ class PlaylistViewController: UIViewController {
             self.playlistImageView.transform = .identity
          })
      }
+    
+    
+    @IBAction func backTapped(_ sender: Any) {
+        ViewController.instance.pauseSound()
+        ViewController.instance.playSound(soundFile: "Eb ")
+    }
 }
